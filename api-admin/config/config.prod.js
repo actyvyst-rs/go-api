9fd3798d3df3527ea0403f181241d85aecf5d76b
actyvyst-rs <ralf.siewert@actyvyst.com>
2019-01-23 23:39:45 +0100

    fixed config.prod

--- api-admin/config/config.prod.js
+++ api-admin/config/config.prod.js
@@ -1,9 +1,6 @@
-const { QUIZ_PORT, QUIZ_MONGO_URI, JWT_SECRET } = process.env;
+const { ADMIN_PORT, ADMIN_MONGO_URI } = process.env;
 
 module.exports = {
-  port: QUIZ_PORT,
-  jwtSecret: JWT_SECRET,
-  jwtTokenExpiration: 900, // 15 min
-  jwtRefreshTokenExpiration: 31536000, //1 year
-  mongoURI: QUIZ_MONGO_URI
+  port: ADMIN_PORT,
+  mongoURI: ADMIN_MONGO_URI
 };
